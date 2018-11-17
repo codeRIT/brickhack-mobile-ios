1418c6985b4d38b446394eb3887a0caf04ddf4b1
Chris Baudouin, Jr <cjb5326@rit.edu>
2018-11-16 23:46:37 -0500

    - Creates additional screens - Implements working buttons for all screens

---
+++ BrickHack-Mobile/ScanTagViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ScanTagViewController.swift
+//  BrickHack-Mobile
+//
+//  Created by Christopher Baudouin, Jr. on 11/16/18.
+//  Copyright © 2018 codeRIT. All rights reserved.
+//
+
+import UIKit
+import CoreNFC
+
+class ScanTagViewController: UIViewController {
+    
+    
+    // Create a scan reader session to allow scanning the wristbands
+    @IBAction func scanButtonPressed(_ sender: Any) {
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
