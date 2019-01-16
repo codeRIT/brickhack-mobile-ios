7e501bf9d57e22303486c17113edb42d0ed3db63
Chris Baudouin, Jr <cjb5326@rit.edu>
2019-01-16 14:25:28 -0500

    - Gets "resource_owner_id" from brickhack.io - Creates base class for TrackableEvent

--- BrickHack-Mobile/ScanHistoryViewController.swift
+++ BrickHack-Mobile/ScanHistoryViewController.swift
@@ -8,12 +8,28 @@
 
 import UIKit
 import OAuth2
+import Alamofire
 
 class ScanHistoryViewController: UIViewController {
 
     var oauth2: OAuth2ImplicitGrant?
+    var sessionManager: SessionManager?
+    var currentUser: Int?
+    let currentUserAPI = "https://staging.brickhack.io/oauth/token/info"
+    let tagsAPI = ""
     
     override func viewDidLoad() {
+        sessionManager = SessionManager()
+        let retrier = OAuth2RetryHandler(oauth2: oauth2!)
+        sessionManager!.adapter = retrier
+        sessionManager!.retrier = retrier
+        sessionManager!.request(currentUserAPI).validate().responseJSON{ response in
+            let _  = self.sessionManager
+            let dict = response.result.value as! [String: Any]
+            self.currentUser = dict["resource_owner_id"] as? Int
+            
+        }
+        
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
---
+++ BrickHack-Mobile/TrackableEvent.swift
@@ -0,0 +1,13 @@
+//
+//  TrackableEvent.swift
+//  BrickHack-Mobile
+//
+//  Created by Christopher Baudouin, Jr. on 1/16/19.
+//  Copyright © 2019 codeRIT. All rights reserved.
+//
+
+import Foundation
+
+class TrackableEvent{
+    
+}
